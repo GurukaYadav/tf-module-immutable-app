bb7446f5a8d29ca13c15fb0c674e1f57933932e5
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-22 12:59:59 +0530

    updating tf-module-immutable-app

--- launch-template.tf
+++ launch-template.tf
@@ -11,7 +11,7 @@ resource "aws_launch_template" "template" {
     market_type = "spot"
   }
 
-  instance_type = "var.INSTANCE_TYPE"
+  instance_type = var.INSTANCE_TYPE
 
 #  Here we are given subnet ids instead of availability zones in which instances can be launched by auto-scaling group
 #  placement {
