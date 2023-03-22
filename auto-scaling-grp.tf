b0806e2b90ad8712ac50955384bf0d0314875ad0
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-22 16:26:25 +0530

    updating tf-module-immutable-app

--- auto-scaling-grp.tf
+++ auto-scaling-grp.tf
@@ -5,8 +5,16 @@ resource "aws_autoscaling_group" "auto_scale" {
   min_size           = var.AGS_MIN
   target_group_arns  = [aws_lb_target_group.target-grp.arn]
 
+  tag {
+    key                 = "Name"
+    value               = "${local.TAG_NAME}-asg"
+    propagate_at_launch = true
+  }
+
   launch_template {
     id      = aws_launch_template.template.id
     version = "$Latest"
   }
+
+
 }
