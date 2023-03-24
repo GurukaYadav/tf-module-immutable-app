53e6ba10a18b2b365c84e4b62654990ec5aee054
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-24 12:39:43 +0530

    updating tf-module-immutable-app

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd /root
 if [ -f /etc/nginx/default.d/roboshop.conf ]; then
   sed -i -e "s/ENV/${ENV}/" /etc/nginx/default.d/roboshop.conf /etc/filebeat/filebeat.yml
   systemctl restart nginx
@@ -8,7 +7,7 @@ if [ -f /etc/nginx/default.d/roboshop.conf ]; then
   exit
 fi
 MEM=$(echo $(free -m  | grep ^Mem | awk '{print $2}')*0.8 |bc | awk -F . '{print $1}')
-sed -i -e "s/ENV/${ENV}/" -e "s/DOCDB_ENDPOINT/${DOCDB_ENDPOINT}/" -e "/java/ s/MEM/$MEM/g" -e "s/REDIS_ENDPOINT/${REDIS_ENDPOINT}/" -e "s/RDS_ENDPOINT/${RDS_ENDPOINT}/" -e "s/DOCDB_USER/${DOCDB_USER}/" -e  "s/RABBITMQ_USER_PASSWORD/${RABBITMQ_USER_PASSWORD}/"  -e  "s/DOCDB_PASS/${DOCDB_PASS}/"
+sed -i -e "s/ENV/${ENV}/" -e "s/DOCDB_ENDPOINT/${DOCDB_ENDPOINT}/" -e "/java/ s/MEM/$MEM/g" -e "s/REDIS_ENDPOINT/${REDIS_ENDPOINT}/" -e "s/RDS_ENDPOINT/${RDS_ENDPOINT}/" -e "s/DOCDB_USER/${DOCDB_USER}/" -e "s/RABBITMQ_USER_PASSWORD/${RABBITMQ_USER_PASSWORD}/"  -e  "s/DOCDB_PASS/${DOCDB_PASS}/"
 /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
 
 systemctl daemon-reload
