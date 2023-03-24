5a4ad357e4d4d5c8027ebbec00ac9e58e85af462
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-24 11:46:41 +0530

    updating tf-module-immutable-app

--- userdata.sh
+++ userdata.sh
@@ -6,8 +6,8 @@ if [ -f /etc/nginx/default.d/roboshop.conf ]; then
   systemctl restart filebeat
   exit
 fi
-#MEM=$(echo $(free -m  | grep ^Mem | awk '{print $2}')*0.8 |bc | awk -F . '{print $1}')
-sed -i -e "s/ENV/${ENV}/" -e "s/DOCDB_ENDPOINT/${DOCDB_ENDPOINT}/" -e "s/REDIS_ENDPOINT/${REDIS_ENDPOINT}/" -e "s/RDS_ENDPOINT/${RDS_ENDPOINT}/" -e "s/DOCDB_USER/${DOCDB_USER}/" -e  "s/RABBITMQ_USER_PASSWORD/${RABBITMQ_USER_PASSWORD}/"  -e  "s/DOCDB_PASS/${DOCDB_PASS}/"
+MEM=$(echo $(free -m  | grep ^Mem | awk '{print $2}')*0.8 |bc | awk -F . '{print $1}')
+sed -i -e "s/ENV/${ENV}/" -e "s/DOCDB_ENDPOINT/${DOCDB_ENDPOINT}/" -e "/java/ s/MEM/$MEM/g" -e "s/REDIS_ENDPOINT/${REDIS_ENDPOINT}/" -e "s/RDS_ENDPOINT/${RDS_ENDPOINT}/" -e "s/DOCDB_USER/${DOCDB_USER}/" -e  "s/RABBITMQ_USER_PASSWORD/${RABBITMQ_USER_PASSWORD}/"  -e  "s/DOCDB_PASS/${DOCDB_PASS}/"
 /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
 
 systemctl daemon-reload
@@ -15,4 +15,3 @@ systemctl enable ${COMPONENT}
 systemctl restart ${COMPONENT}
 systemctl restart filebeat
 
-  --//
